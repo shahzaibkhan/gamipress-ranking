8d22bf91032f54ba814949f8841567f42f21207d
Rafael Carneiro de Moraes <rafinhacarneiro@users.noreply.github.com>
2019-01-14 14:47:46 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gamipress-ranking
---
+++ gamipress-ranking.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Gamipress Ranking
+ *
+ * @package     gamipress-ranking
+ * @author      Rafael Carneiro de Moraes
+ * @copyright   2019 Prime Control
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Gamipress Ranking
+ * Plugin URI:  https://academy.primecontrol.com.br/gamipress-ranking
+ * Description: Ranking para o Gamipress.
+ * Version:     1.0.0
+ * Author:      Rafael Carneiro de Moraes
+ * Author URI:  https://rafinhacarneiro.github.io
+ * Text Domain: gamipress-ranking
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+?>
